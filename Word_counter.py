dfa282d3b01bdb37d8950fe6efaf943a70d179e9
Kamesh10sk <167958086+Kamesh10sk@users.noreply.github.com>
2025-02-05 17:02:42 +0530

    Word_counter.py

--- Word_counter.py
+++ Word_counter.py
@@ -1,7 +1,6 @@
 def count_words(text: str) -> int:
     return len(text.split())
 def main():
-    print("Welcome to the Word Counter Program!")
     user_input = input("Please enter a sentence or paragraph: ").strip()
     if not user_input:
         print("Error: No input provided. Please enter a valid sentence or paragraph.")
